1586b16bc376fce0cb42709fc352ed163f342447
wangzhenyu946 <923680072@qq.com>
2021-03-23 19:27:21 +0800

    作用域存储的理解

--- app/src/main/java/com/wangzhenyu/catwatch/fragment/PhotoFragment.kt
+++ app/src/main/java/com/wangzhenyu/catwatch/fragment/PhotoFragment.kt
@@ -1,9 +1,18 @@
 package com.wangzhenyu.catwatch.fragment
 
+import android.Manifest
+import android.content.ContentUris
+import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
+import android.provider.MediaStore
 import android.view.*
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
@@ -11,6 +20,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
 import com.wangzhenyu.catwatch.R
 import com.wangzhenyu.catwatch.adapter.PhotoListAdapter
 import com.wangzhenyu.catwatch.data.GirlPhoto
@@ -19,7 +32,13 @@ import com.wangzhenyu.catwatch.util.LogUtil
 import com.wangzhenyu.catwatch.util.MyApplication
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
 import java.io.File
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 class PhotoFragment : Fragment() {
 
@@ -37,10 +56,20 @@ class PhotoFragment : Fragment() {
                 "如果您没有声明任何危险权限，或者您的应用安装在搭载 Android 5.1（API 级别 22）或更低版本的设备上，则系统会自动授予相应的权限，您无需完成本页剩下的任何步骤。\n" +
                 "\n" +
                 "基本原则"
+
+        const val uri: String =
+            "https://cn.bing.com/th?id=OHR.LoftedMadagascar_ZH-CN0062899981_1920x1080.jpg&amp;rf=LaDigue_1920x1080.jpg&amp;pid=hp"
+        const val uri1 =
+            "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhuafans.dbankcloud.com%2Fpic%2F2017%2F01%2F25%2F195116eebdd5aa2fdaebff832280a391_IMG_20170125_153017.jpg%3Fmode%3Ddownload&refer=http%3A%2F%2Fhuafans.dbankcloud.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619073600&t=a6668fff195cd5b608e40c328b189b27"
+        const val uri2 = ""
     }
 
     private lateinit var viewModel: PhotoViewModel
     private lateinit var binding: FPhotoFragmentBinding
+
+    private lateinit var adapter: PhotoListAdapter
+
+    private val list = ArrayList<GirlPhoto>()
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
@@ -51,110 +80,314 @@ class PhotoFragment : Fragment() {
         return when (item.itemId) {
 
             R.id.create_file -> {
-
                 lifecycleScope.launch(Dispatchers.IO)
                 {
                     try {
-                        requireContext().openFileOutput(
-                            internalStorageFileName,
-                            Context.MODE_PRIVATE
-                        ).buffered().use {
-                            it.write(internalStorageFileContent.toByteArray(Charsets.UTF_8))
+                        File(
+                            requireContext().getDir("photo", Context.MODE_PRIVATE),
+                            "create_internal.png"
+                        ).outputStream().buffered().use {
+                            val bis = BufferedInputStream(loadPhoto(uri))
+                            val buffer = ByteArray(1024 * 8)
+                            var bytes = bis.read(buffer)
+                            while (bytes >= 0) {
+                                it.write(buffer, 0, bytes)
+                                bytes = bis.read(buffer)
+                            }
+                            bis.close()
                         }
                     } catch (e: Exception) {
-                        LogUtil.d("StorageABC", "读取失败")
+                        LogUtil.d("StorageABC", "创建内部存储文件失败")
                     }
-
-                    try {
-                        File(context?.cacheDir, "123.txt").outputStream().bufferedWriter()
-                            .use {
-                                it.write("外部存储分为两部分：自带外部存储和扩展外部存储(外置SD卡)")
-                            }
-                    } catch (e: Exception) {
-
-                    }
-
                 }
                 true
             }
             R.id.open_file -> {
                 lifecycleScope.launch(Dispatchers.IO)
                 {
-                    try {
-                        requireContext().openFileInput(
-                            internalStorageFileName
-                        ).bufferedReader().useLines {
-                            val a = it.fold("WZY") { a, b ->
-                                "$a $b"
-                            }
-
-                            LogUtil.d("StorageABC", a)
-                        }
-                    } catch (e: Exception) {
-                        LogUtil.d("StorageABC", "读取失败")
-                    }
-                }
-
-                val files: Array<String> = requireContext().fileList()
-                LogUtil.d("StorageABC", files.contentToString())
-                try {
-                    File(requireContext().cacheDir, "123.txt")
-                        .inputStream().bufferedReader()
-                        .use {
-                            val a = it.readText()
-                            LogUtil.d("StorageABC", a)
-                        }
-
-                } catch (e: Exception) {
+                    val openFileUrl: String =
+                        "${requireContext().filesDir.path}/create_internal.png"
+                    LogUtil.d("StorageABC", openFileUrl)
+                    list.add(GirlPhoto(openFileUrl))
+                    adapter.submitList(list)
                 }
                 true
             }
 
             R.id.create_file_external -> {
-
                 lifecycleScope.launch(Dispatchers.IO)
                 {
                     try {
-                        val appSpecificExternalDir =
-                            File(
-                                ContextCompat.getExternalCacheDirs(MyApplication.context)[1],
-                                "111.txt"
-                            ).outputStream()
-                                .bufferedWriter().use {
-                                    it.write(internalStorageFileContent)
+                        File(requireContext().getExternalFilesDir("photo"), "external.png")
+                            .outputStream().buffered().use {
+                                val bis = BufferedInputStream(loadPhoto(uri))
+                                val buffer = ByteArray(1024 * 8)
+                                var bytes = bis.read(buffer)
+                                while (bytes >= 0) {
+                                    it.write(buffer, 0, bytes)
+                                    bytes = bis.read(buffer)
                                 }
-
+                                bis.close()
+                            }
                     } catch (e: Exception) {
-
+                        LogUtil.d("StorageABC", "创建外部存储文件失败")
                     }
-                }
 
+                }
                 true
             }
             R.id.open_file_external -> {
                 lifecycleScope.launch(Dispatchers.IO)
                 {
-                    File(ContextCompat.getExternalCacheDirs(MyApplication.context)[1], "111.txt")
-                        .inputStream().bufferedReader()
-                        .useLines {
-                            val x = it.fold("Wzy") { a, b ->
-                                "$a$b"
-                            }
-                            LogUtil.d("StorageABC", x)
-                        }
+                    val externalURL =
+                        "${requireContext().getExternalFilesDir("photo")}/external.png"
+                    list.clear()
+                    list.add(GirlPhoto(externalURL))
+                    adapter.submitList(list)
                 }
 
                 true
             }
 
+            R.id.photo_menu_one -> {
+                lifecycleScope.launch(Dispatchers.IO)
+                {
+                    if (Build.VERSION.SDK_INT > 23) {
 
-            R.id.photo_menu_share -> {
-                LogUtil.d("StorageABC", "share")
+                        val checkList = arrayOf(
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        )
+                        val needRequestPermission = checkPermission(requireContext(), checkList)
+                        LogUtil.d("StorageABC", needRequestPermission.toString())
+                        if (needRequestPermission.isEmpty()) {
+                            LogUtil.d("StorageABC", "已经获得所有权限")
+                            getPhoto()
+                        } else {
+                            requestPermissions(needRequestPermission.toTypedArray(), 1)
+                        }
+                    } else {
+                        getPhoto()
+                    }
+
+                }
+
+//                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+//                intent.addCategory(Intent.CATEGORY_OPENABLE)
+//                intent.type = "image/jpeg"
+//                startActivityForResult(intent, 100)
+
+                true
+            }
+            R.id.photo_menu_two -> {
+                lifecycleScope.launch(Dispatchers.IO)
+                {
+                    val list: ArrayList<GirlPhoto> =
+                        arguments?.getParcelableArrayList("PhotoList")!!
+                    val position: Int = arguments?.getInt("PhotoPosition") ?: 0
+                    val url: String = list[position].url
+                    Glide.with(this@PhotoFragment).asBitmap().load(url)
+                        .into(object : CustomTarget<Bitmap>() {
+                            override fun onResourceReady(
+                                resource: Bitmap,
+                                transition: Transition<in Bitmap>?
+                            ) {
+                                addBitmapToAlbum(
+                                    resource,
+                                    "photo${position}.png",
+                                    "image/png",
+                                    Bitmap.CompressFormat.PNG
+                                )
+                            }
+
+                            override fun onLoadCleared(placeholder: Drawable?) {
+                            }
+
+                        })
+                }
+                true
+            }
+            R.id.photo_menu_three -> {
+                lifecycleScope.launch(Dispatchers.IO)
+                {
+
+                }
+                true
+            }
+            R.id.photo_menu_four -> {
+                lifecycleScope.launch(Dispatchers.IO)
+                {
+
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    //将位图添加到图库中
+    private fun addBitmapToAlbum(
+        bitmap: Bitmap,
+        displayName: String,
+        mimeType: String,
+        compressFormat: Bitmap.CompressFormat
+    ) {
+        val values = ContentValues()
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, displayName)
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+        } else {
+            values.put(
+                MediaStore.MediaColumns.DATA,
+                "${Environment.getExternalStorageDirectory().path}/${Environment.DIRECTORY_DCIM}/$displayName"
+            )
+        }
+        val uri = MyApplication.context.contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values
+        )
+        if (uri != null) {
+            val outputStream = MyApplication.context.contentResolver.openOutputStream(uri)
+            if (outputStream != null) {
+                bitmap.compress(compressFormat, 100, outputStream)
+                outputStream.close()
+            }
+        }
+    }
+
+    //检查权限，返回需要申请的权限
+    private fun checkPermission(context: Context, checkList: Array<String>): ArrayList<String> {
+        val list: ArrayList<String> = ArrayList<String>()
+        for (i in 1..checkList.size) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(
+                    context,
+                    checkList[i - 1]
+                )
+            ) {
+                list.add(checkList[i - 1])
+            }
+        }
+        return list
+    }
+
+    //将图片添加到图库中
+    private fun writeInputStreamToAlbum(
+        inputStream: InputStream,
+        displayName: String,
+        mimeType: String
+    ) {
+        val values = ContentValues()
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, displayName)
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+        } else {
+            values.put(
+                MediaStore.MediaColumns.DATA,
+                "${Environment.getExternalStorageDirectory().path}/${Environment.DIRECTORY_DCIM}/$displayName"
+            )
+        }
+        val bis = BufferedInputStream(inputStream)
+        val uri = MyApplication.context.contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values
+        )
+        if (uri != null) {
+            val outputStream = MyApplication.context.contentResolver.openOutputStream(uri)
+            if (outputStream != null) {
+                val bos = BufferedOutputStream(outputStream)
+                val buffer = ByteArray(1024)
+                var bytes = bis.read(buffer)
+                while (bytes >= 0) {
+                    bos.write(buffer, 0, bytes)
+                    bos.flush()
+                    bytes = bis.read(buffer)
+                }
+                bos.close()
+            }
+        }
+        bis.close()
+    }
+
+    //从网络上加载图片
+    private fun loadPhoto(url: String): InputStream {
+        val conn: HttpURLConnection = (URL(url).openConnection() as HttpURLConnection)
+        conn.connect()
+        return conn.inputStream
+    }
+
+    private fun getBitmap(url: String): Bitmap {
+        val conn: HttpURLConnection = (URL(url).openConnection() as HttpURLConnection)
+        conn.connect()
+        val inputStream: InputStream = conn.inputStream
+        if (inputStream == null) {
+            LogUtil.d("AAAA", "inputStream为空")
+        }
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        conn.disconnect()
+        return bitmap
+    }
+
+    private fun getPhoto() {
+        val list: ArrayList<GirlPhoto> = ArrayList<GirlPhoto>()
+        val cursor =
+            MyApplication.context.contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val id =
+                    cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
+                val uri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                list.add(GirlPhoto(uri.toString()))
+            }
+            cursor.close()
+            adapter.submitList(list)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === 100) {
+            //选中返回的图片封装在uri里
+            LogUtil.d("StorageABC", data?.data?.toString() ?: "您未获取图片")
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty()) {
+                    LogUtil.d("StorageABC", grantResults.contentToString())
+                    var a: Boolean = true
+                    for (i in grantResults) {
+                        if (i != PackageManager.PERMISSION_GRANTED) {
+                            a = false
+                        }
+                    }
+                    if (!a) {
+                        LogUtil.d("StorageABC", "未全部获取权限")
+                    } else {
+                        getPhoto()
+                    }
+                } else {
+                    LogUtil.d("StorageABC", "未获取权限")
+                }
+            }
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -177,10 +410,10 @@ class PhotoFragment : Fragment() {
         binding.photoFragmentTextView.text =
             "${arguments?.getInt("PhotoPosition")?.plus(1) ?: 0}/${list.size ?: 0}"
 
-        PhotoListAdapter().let {
-            binding.photoFragmentViewPager2.adapter = it
-            it.submitList(list)
-        }
+        adapter = PhotoListAdapter()
+        binding.photoFragmentViewPager2.adapter = adapter
+        adapter.submitList(list)
+
 
         binding.photoFragmentViewPager2.apply {
             setCurrentItem(arguments?.getInt("PhotoPosition") ?: 0, false)
